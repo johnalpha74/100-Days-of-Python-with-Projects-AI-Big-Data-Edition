940255167ea75e6a882a8ea9df0b7d661d14a603
johnalpha74 <johnalpha@gmail.com>
2025-08-07 22:29:24 +0200

    CHORE: Declare Variables

---
+++ Day_01/string.py
@@ -0,0 +1,12 @@
+# Declare variables
+
+name = 'John'
+carNumber = 25
+is_man = True
+pressure = 29.089
+is_binary = 0B1
+
+print("You name is ", name, " | data type is ", type(name))
+print(name, "is a man: ", is_man, " | data type is ", type(is_man))
+print("The pressure is ", pressure, " } data type is :", type(pressure))
+
